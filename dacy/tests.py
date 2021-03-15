3110197d139c06cda11ec943b4b6c0e9516c1168
KennethEnevoldsen <kennethcenevolsen@gmail.com>
2021-03-15 12:08:21 +0100

    added test

--- dacy/tests.py
+++ dacy/tests.py
@@ -2,6 +2,7 @@ import urllib
 import os
 
 from download import models_url
+from load import load
 
 def test_urls():
     for m, url in models_url.items():
@@ -11,3 +12,9 @@ def test_urls():
         print("\t Status:", f.status)
         size = int(f.headers["Content-Length"]) / 1e6
         print("\t File Size:", round(size), "mb")
+
+
+def test_load():
+    for m, url in models_url.items():
+        nlp = load(m)
+        nlp("Dette er en test tekst")
