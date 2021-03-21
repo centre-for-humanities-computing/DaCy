3daef452ce979c3093dd1a5203d25f5f48753776
KennethEnevoldsen <kennethcenevolsen@gmail.com>
2021-03-21 14:24:18 +0100

    Minor edits
    Co-authored-by: MalteHB <MalteHB@users.noreply.github.com>

---
+++ dacy/readability/readability.py
@@ -0,0 +1,15 @@
+def LIX_getter(doc):
+    """
+    extract LIX
+
+    Example:
+    Doc.set_extension("LIX", getter=LIX_getter)
+    # fetch result
+    doc._.LIX
+    """
+    O = len(doc)
+    P = len(list(doc.sents))
+    L = len([t for t in doc if len(t) > 6])
+
+    LIX = O / P + L * 100 / O
+    return LIX
