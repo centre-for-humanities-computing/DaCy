2d28921c0c9f4d9db17b4e86e14b8a55a95b9240
KennethEnevoldsen <kennethcenevolsen@gmail.com>
2021-03-26 16:00:10 +0100

    minor stuff

--- setup.py
+++ setup.py
@@ -16,9 +16,9 @@ setuptools.setup(
     author_email="kennethcenevoldsen@gmail.com",
     url="https://github.com/KennethEnevoldsen/dacy",
     packages=setuptools.find_packages(),
-    data_files=[("", ["dacy/*.csv"])],
+    include_package_data=True,
     # external packages as dependencies
-    install_requires=["spacy", "spacy-transformers", "tqdm"],
+    install_requires=["spacy", "spacy-transformers", "tqdm", "danlp"],
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         # How mature is this project? Common values are
