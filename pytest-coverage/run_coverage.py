3ed7d0f98795444898234ffe2533799c4b5a9cfe
KennethEnevoldsen <kennethcenevolsen@gmail.com>
2021-06-01 14:19:10 +0200

    fixed coverage script

--- pytest-coverage/run_coverage.py
+++ pytest-coverage/run_coverage.py
@@ -6,11 +6,15 @@ import subprocess
 from pathlib import Path
 
 
-output = subprocess.check_output("pytest --cov=dacy --cov-config=pytest-coverage/.coveragerc --cov-report term-missing", shell=True)
+output = subprocess.check_output(
+    "pytest --cov=dacy --cov-config=pytest-coverage/.coveragerc --cov-report term-missing",
+    shell=True,
+)
 
-save_path = ".github/pytest-coverage"
+save_path = "pytest-coverage"
 Path(save_path).mkdir(parents=True, exist_ok=True)
 
 save_path = os.path.join(save_path, "pytest-coverage.txt")
+os.remove(save_path)
 with open(save_path, "w") as f:
-    f.write(output)
+    f.write(output.decode("utf-8"))
