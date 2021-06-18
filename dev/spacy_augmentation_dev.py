2166cf43a10c05261a26576f458caf8895697249
Lasse <lasseh0310@gmail.com>
2021-06-18 12:01:33 +0200

    fancy name augmenter more or less done

--- dev/spacy_augmentation_dev.py
+++ dev/spacy_augmentation_dev.py
@@ -3,13 +3,95 @@ This includes a series of SpaCy Augmenters
 """
 from functools import partial
 from math import inf
-from typing import Callable, Iterator, List
+from typing import Callable, Iterator, List, Union
 
 import spacy
 from spacy.training import Example
 import random
 from spacy.training.augment import create_lower_casing_augmenter
 from spacy.language import Language
+
+
+##########
+# TODO
+# Make augment_entity() choose random pattern within example
+##########
+
+"""
+Augmenter function
+"""
+
+
+@spacy.registry.augmenters("name_augmenter.v1")
+def create_name_augmenter(
+    ent_dict: dict,
+    patterns: List[str] = ["fn,ln", "abbpunct,ln"],
+    patterns_prob: List[float] = None,
+    force_size: bool = False,
+    keep_name: bool = True,
+    prob: float = 1,
+) -> Callable[[Language, Example], Iterator[Example]]:
+    """Create name augmenter
+
+    Args:
+        ent_dict (dict): A dictionary of first_name and last_name to replace with
+        patterns (List[float, optional): The patterns to replace with.
+            Will choose one at random, optionally weighted by pattern_probs
+            Options: "fn", "ln", "abb", "abbpunct". Defaults to ["fn,ln","abbpunct,ln"].
+        patterns_prob (List[float]). Weights for the patterns. Defaults to None (equal weights)
+        force_size (bool, optional): Whether to force entities have the same format as pattern. Defaults to False.
+        keep_name (bool, optional): Whether to use the current name or sample from ent_dict. Defaults to True.
+        prob (float, optional): which proportion of entities to augment. Defaults to 1.
+
+    Returns:
+        Callable[[Language, Example], Iterator[Example]]
+    """
+    return partial(
+        name_augmenter,
+        ent_dict=ent_dict,
+        patterns=patterns,
+        patterns_prob=patterns_prob,
+        force_size=force_size,
+        keep_name=keep_name,
+        prob=prob,
+    )
+
+
+def name_augmenter(
+    nlp: Language,
+    example: Example,
+    ent_dict: dict,
+    patterns: list,
+    patterns_prob: Union[bool, List[float]],
+    force_size: bool,
+    keep_name: bool,
+    prob: float,
+) -> Iterator[Example]:
+    ex_dict = example.to_dict()
+
+    # Get slices containing names
+    entity_slices = get_ent_slices(ex_dict["doc_annotation"]["entities"])
+    # Extract tokens corresponding to names
+    name_tokens = get_slice_spans(ex_dict["token_annotation"]["ORTH"], entity_slices)
+
+    # add possibility to sample from different patterns
+
+    # more args
+    aug_ents = augment_entity(
+        entities=name_tokens,
+        ent_dict=ent_dict,
+        patterns=patterns,
+        patterns_prob=patterns_prob,
+        force_size=force_size,
+        keep_name=keep_name,
+        prob=prob,
+    )
+    # Update fields in example dictionary to match changes
+    up_ex_dict = update_spacy_properties(ex_dict, aug_ents, entity_slices)
+    text = make_text_from_orth(up_ex_dict)
+
+    doc = nlp.make_doc(text)
+    yield example.from_dict(doc, up_ex_dict)
 
 
 """
@@ -73,6 +155,170 @@ def get_slice_spans(l: List[str], slices: List[tuple]):
     return [l[slice(s[0], s[1])] for s in slices]
 
 
+def update_spacy_properties(
+    example_dict: dict,
+    augmented_entities: List[List[str]],
+    entity_slices: List[tuple],
+) -> dict:
+
+    for k, v in example_dict["token_annotation"].items():
+        example_dict["token_annotation"][k] = update_slice(
+            k, v, augmented_entities, entity_slices
+        )
+    example_dict["doc_annotation"]["entities"] = update_slice(
+        "entities",
+        example_dict["doc_annotation"]["entities"],
+        augmented_entities,
+        entity_slices,
+    )
+    return example_dict
+
+
+def update_slice(
+    type: str,
+    values: List[str],
+    aug_ents: List[List[str]],
+    entity_slices: List[tuple],
+) -> List[str]:
+    if type == "ORTH":
+        return handle_orth(values, aug_ents, entity_slices)
+    elif type == "SPACY":
+        return handle_spacy(values, aug_ents, entity_slices)
+    elif type == "TAG":
+        return handle_tag(values, aug_ents, entity_slices)
+    elif type == "LEMMA":
+        return handle_lemma(values, aug_ents, entity_slices)
+    elif type == "POS":
+        return handle_pos(values, aug_ents, entity_slices)
+    elif type == "MORPH":
+        return handle_morph(values, aug_ents, entity_slices)
+    elif type == "HEAD":
+        return handle_head(values, aug_ents, entity_slices)
+    elif type == "DEP":
+        return handle_dep(values, aug_ents, entity_slices)
+    elif type == "SENT_START":
+        return handle_sent_start(values, aug_ents, entity_slices)
+    elif type == "entities":
+        return handle_entities(values, aug_ents, entity_slices)
+
+
+"""
+Handlers for spacy properties
+"""
+
+
+def handle_orth(
+    values: List[str], aug_ents: List[List[str]], entity_slices: List[tuple]
+) -> List[str]:
+
+    running_add = 0
+    for i, s in enumerate(entity_slices):
+        values[slice(s[0] + running_add, s[1] + running_add)] = aug_ents[i]
+        running_add += len(aug_ents[i]) - (s[1] - s[0])
+    return values
+
+
+def handle_spacy(
+    values: List[str], aug_ents: List[List[str]], entity_slices: List[tuple]
+) -> List[str]:
+    running_add = 0
+    for i, s in enumerate(entity_slices):
+        values[slice(s[0] + running_add, s[1] + running_add)] = [True] * len(
+            aug_ents[i]
+        )
+        running_add += len(aug_ents[i]) - (s[1] - s[0])
+    return values
+
+
+def handle_tag(
+    values: List[str], aug_ents: List[List[str]], entity_slices: List[tuple]
+) -> List[str]:
+    running_add = 0
+    for i, s in enumerate(entity_slices):
+        values[slice(s[0] + running_add, s[1] + running_add)] = ["PROPN"] * len(
+            aug_ents[i]
+        )
+        running_add += len(aug_ents[i]) - (s[1] - s[0])
+    return values
+
+
+def handle_lemma(
+    values: List[str], aug_ents: List[List[str]], entity_slices: List[tuple]
+) -> List[str]:
+    return handle_orth(values, aug_ents, entity_slices)
+
+
+def handle_pos(
+    values: List[str], aug_ents: List[List[str]], entity_slices: List[tuple]
+) -> List[str]:
+    """keep first pos tag as original, add flat to rest"""
+    running_add = 0
+    for i, s in enumerate(entity_slices):
+        values[slice(s[0] + running_add, s[1] + running_add)] = [
+            values[slice(s[0] + running_add, s[1] + running_add)][0]
+        ] + ["flat"] * (len(aug_ents[i]) - 1)
+        running_add += len(aug_ents[i]) - (s[1] - s[0])
+    return values
+
+
+def handle_morph(
+    values: List[str], aug_ents: List[List[str]], entity_slices: List[tuple]
+) -> List[str]:
+    running_add = 0
+    for i, s in enumerate(entity_slices):
+        values[slice(s[0] + running_add, s[1] + running_add)] = [""] * len(aug_ents[i])
+        running_add += len(aug_ents[i]) - (s[1] - s[0])
+    return values
+
+
+def handle_head(
+    values: List[str], aug_ents: List[List[str]], entity_slices: List[tuple]
+) -> List[str]:
+    """keep first head, set rest to refer to index of first name"""
+    running_add = 0
+    for i, s in enumerate(entity_slices):
+        values[slice(s[0] + running_add, s[1] + running_add)] = [
+            values[slice(s[0] + running_add, s[1] + running_add)][0]
+        ] + [s[0] + running_add] * (len(aug_ents[i]) - 1)
+        running_add += len(aug_ents[i]) - (s[1] - s[0])
+    return values
+
+
+def handle_dep(
+    values: List[str], aug_ents: List[List[str]], entity_slices: List[tuple]
+) -> List[str]:
+    return handle_pos(values, aug_ents, entity_slices)
+
+
+def handle_sent_start(
+    values: List[str], aug_ents: List[List[str]], entity_slices: List[tuple]
+) -> List[str]:
+    """keep first (if sent start), set rest to 0"""
+    running_add = 0
+    for i, s in enumerate(entity_slices):
+        values[slice(s[0] + running_add, s[1] + running_add)] = [
+            values[slice(s[0] + running_add, s[1] + running_add)][0]
+        ] + [0] * (len(aug_ents[i]) - 1)
+        running_add += len(aug_ents[i]) - (s[1] - s[0])
+    return values
+
+
+def handle_entities(
+    values: List[str], aug_ents: List[List[str]], entity_slices: List[tuple]
+) -> List[str]:
+    running_add = 0
+    for i, s in enumerate(entity_slices):
+        len_ents = s[1] - s[0]
+        if len_ents == 1:
+            values[slice(s[0] + running_add, s[1] + running_add)] = ["U-PER"]
+        else:
+            values[slice(s[0] + running_add, s[1] + running_add)] = (
+                ["B-PER"] + ["I-PER"] * (len_ents - 2) + ["L-PER"]
+            )
+        running_add += len(aug_ents[i]) - (s[1] - s[0])
+    return values
+
+
 """
 Augment
 """
@@ -81,9 +327,10 @@ Augment
 def augment_entity(
     entities: List[List[str]],
     ent_dict: dict,
-    pattern: str = "fn,ln",
+    patterns: List[str] = ["fn,ln", "abbpunct,ln"],
+    patterns_prob: List[float] = None,
     force_size: bool = False,
-    keep_name: bool = False,
+    keep_name: bool = True,
     prob: float = 1,
 ) -> List[List[str]]:
     """Augment entities
@@ -91,28 +338,33 @@ def augment_entity(
     Args:
         entities (List[List[str]]): A list of lists of spans of entities/names to replace
         ent_dict (dict): A dictionary of first_name and last_name to replace with
-        pattern (str, optional): The pattern to replace with.
-            Options: "fn", "ln", "abb", "abbpunct". Defaults to "fn,ln".
-        force_size (bool, optional): Whether to force entities have the same format as pattern
-        keep_name (bool, optional): Whether to use the current name or sample from ent_dict. Defaults to False.
-        prob (float, optional): which proportion of entities to augment
-
+        patterns (List[float, optional): The patterns to replace with.
+            Will choose one at random, optionally weighted by pattern_probs
+            Options: "fn", "ln", "abb", "abbpunct". Defaults to ["fn,ln","abbpunct,ln"].
+        patterns_prob (List[float]). Weights for the patterns. Defaults to None (equal weights)
+        force_size (bool, optional): Whether to force entities have the same format as pattern. Defaults to False.
+        keep_name (bool, optional): Whether to use the current name or sample from ent_dict. Defaults to True.
+        prob (float, optional): which proportion of entities to augment. Defaults to 1.
 
     Returns:
         List[List[str]]: [description]
     """
+    if isinstance(patterns, str):
+        patterns = [patterns]
+
+    pattern = random.choices(patterns, weights=patterns_prob, k=1)[0]
     pattern = pattern.split(",")
 
     new_entity_spans = []
 
     for entity_span in entities:
         if force_size:
-            entity_span = resize_entity_list(entity_span, pattern)
+            entity_span = resize_entity_list(entity_span, pattern, ent_dict)
 
         new_entity = []
 
         for i, ent in enumerate(entity_span):
-            if random.random > prob:
+            if random.random() > prob:
                 new_entity.append(ent)
             else:
                 if i > len(pattern):
@@ -143,6 +395,26 @@ def resize_entity_list(
         ]
 
 
+def make_text_from_orth(example_dict: dict) -> str:
+    """
+    Reconstructs the text based on the changes made to ORTH
+    """
+    text = ""
+    for orth, spacy in zip(
+        example_dict["token_annotation"]["ORTH"],
+        example_dict["token_annotation"]["SPACY"],
+    ):
+        text += orth
+        if spacy:
+            text += " "
+    return text
+
+
+"""
+Testing
+"""
+
+
 def make_ent_dict():
     return {
         "first_name": ["Johnny", "Birte", "Tony"],
@@ -150,16 +422,35 @@ def make_ent_dict():
     }
 
 
-ent_dict = make_ent_dict()
+if _name__ == "__main__":
 
-nlp = spacy.load("da_core_news_sm")
-doc = nlp("Mit navn er Kenneth Enevoldsen, og Lasse, Jakob og Kenneth.")
-example = Example(doc, doc)
-ex_dict = example.to_dict()
+    ent_dict = make_ent_dict()
 
-entity_slices = get_ent_slices(ex_dict["doc_annotation"]["entities"])
-orth = get_slice_spans(ex_dict["token_annotation"]["ORTH"], entity_slices)
+    nlp = spacy.load("da_core_news_sm")
+    doc = nlp("Mit navn er Kenneth Henrik Enevoldsen, og Lasse, Jakob og Kenneth.")
+    example = Example(doc, doc)
+    ex_dict = example.to_dict()
 
-augment_entity(orth, ent_dict, force_size=True)
-augment_entity(orth, ent_dict)
-augment_entity(orth, ent_dict, pattern="abbpunct,ln,fn,ln,ln", force_size=False)
+    entity_slices = get_ent_slices(ex_dict["doc_annotation"]["entities"])
+    orth = get_slice_spans(ex_dict["token_annotation"]["ORTH"], entity_slices)
+
+    # add possibility to sample from different patterns
+
+    aug_ents = augment_entity(orth, ent_dict, force_size=True)
+    ent_lens = [len(ents) for ents in aug_ents]
+
+    aug_ent = augment_entity(orth, ent_dict, force_size=True)
+
+    up_ex_dict = update_spacy_properties(ex_dict, aug_ent, entity_slices)
+
+    _orth = ex_dict["token_annotation"]["ORTH"]
+
+    running_add = 0
+    for i, s in enumerate(entity_slices):
+        _orth[slice(s[0] + running_add, s[1] + running_add)] = aug_ents[i]
+        running_add += len(aug_ents[i]) - (s[1] - s[0])
+
+    _orth
+
+    augment_entity(orth, ent_dict)
+    augment_entity(orth, ent_dict, pattern="abbpunct,ln,fn,ln,ln", force_size=False)
