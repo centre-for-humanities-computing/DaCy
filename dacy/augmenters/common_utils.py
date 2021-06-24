a848f9a79fda47c33bd79a2cf861df10185e5665
Lasse <lasseh0310@gmail.com>
2021-06-24 15:35:00 +0200

    add nb, some moving around

--- dacy/augmenters/common_utils.py
+++ dacy/augmenters/common_utils.py
@@ -1,4 +1,4 @@
-"""A bunch of utilities function that can be reused across different augmenters"""
+"""A bunch of utilities functions that can be reused across different augmenters"""
 import pandas as pd
 
 
@@ -30,7 +30,8 @@ def make_test_ent_dict():
 
 
 def make_muslim_name_dict():
-    names = pd.read_csv("../lookup_tables/names.csv")
+
+    names = pd.read_csv("lookup_tables/names.csv")
     first_names = names.loc[
         (names["ethnicity"] == "muslim") & (names["first_name"] == True)
     ]
@@ -44,7 +45,8 @@ def make_muslim_name_dict():
 
 
 def make_danish_name_dict():
-    names = pd.read_csv("../lookup_tables/names.csv")
+
+    names = pd.read_csv("lookup_tables/names.csv")
     first_names = names.loc[
         (names["ethnicity"] == "danish") & (names["first_name"] == True)
     ]
