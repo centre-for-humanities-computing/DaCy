d68a056e2b21e8759c4a3f8ea1a60ff781467fdd
KennethEnevoldsen <kennethcenevolsen@gmail.com>
2021-06-24 21:08:11 +0200

    added dummmy tests for augmenters

---
+++ dacy/tests/augmenters/test_keyboard.py
@@ -0,0 +1,14 @@
+from dacy.augmenters.keyboard import Keyboard, qwerty_da_array, make_keyboard_augmenter
+
+def test_Keyboard():
+    kb = Keyboard(keyboard_array = qwerty_da_array)
+
+    assert kb.coordinate("q") == (1, 1)
+    assert kb.is_shifted()("q") is False
+    assert kb.euclidian_distance("q", "a") <= 1
+    assert len(set(kb.all_keys())) > 28*2
+    assert "w" in kb.get_neighboors("q")
+    kb.create_distance_dict()
+
+def test_make_keyboard_augmenter():
+    augmenter = make_keyboard_augmenter(doc_level=1, char_level=0.5)
