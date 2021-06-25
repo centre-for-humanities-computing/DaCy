c818288ae89b5638e5a417ceb8e525666a8a0330
Lasse <lasseh0310@gmail.com>
2021-06-25 10:30:03 +0200

    fix loading of names csv

--- setup.py
+++ setup.py
@@ -25,7 +25,6 @@ setuptools.setup(
     author_email="kennethcenevoldsen@gmail.com",
     url="https://github.com/KennethEnevoldsen/dacy",
     packages=setuptools.find_packages(),
-    data_files=[("", ["dacy/datasets/lookup_tables/*.csv"])],
     include_package_data=True,
     # external packages as dependencies
     install_requires=requirements,
