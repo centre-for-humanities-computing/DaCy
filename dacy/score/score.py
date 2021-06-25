c89d179202a76712f30395982b649b1bb1af7e48
KennethEnevoldsen <kennethcenevolsen@gmail.com>
2021-06-25 11:57:58 +0200

    updated scores for list of augmenters

--- dacy/score/score.py
+++ dacy/score/score.py
@@ -13,6 +13,7 @@ from pydantic import BaseModel
 from spacy.language import Language
 from spacy.scorer import Scorer
 from spacy.training import Corpus
+from spacy.training import dont_augment
 
 from ..utils import flatten_dict
 
@@ -41,7 +42,7 @@ class Scores(BaseModel):
     def __repr_str__(self, join_str: str) -> str:
         return join_str.join(
             repr(v) if a is None else f"{a}={v!r}"
-            for a, v in [(k, self.summary(k)) for k in self.scores.keys()]
+            for a, v in [(k, self.summary(k)) for k in self.scores.keys() if isinstance(self.scores[k][0], float)]
         )
 
     def __add__(self, other: Scores):
@@ -56,9 +57,9 @@ class Scores(BaseModel):
 
 def score(
     corpus: Corpus,
-    apply_fn: Callable,
+    apply_fn: Union[Callable, Language],
     score_fn: List[Union[Callable, str]] = ["token", "pos", "ents"],
-    augmenter: Union[List[Callable], Callable] = [],
+    augmenters: Union[List[Callable], Callable] = [],
     k: int = 1,
     nlp: Optional[Language] = None,
     **kwargs,
@@ -67,15 +68,16 @@ def score(
 
     Args:
         corpus (Corpus): A spacy Corpus
-        apply_fn (Callable): A wrapper function for the model you wish to score. The model should
-            take in a spacy Example and output a tagged version of it.
+        apply_fn (Union[Callable, Language]): A wrapper function for the model you wish to score. The model should
+            take in a spacy Example and output a tagged version of it. A SpaCy pipeline can be provided as is and a
+            wrapper will be created for it.
         score_fn (List[Union[Callable, str]], optional): A scoring function which takes in a list of
             examples and return a dictionary of the form {"score_name": score}.Four potiential
             strings are valid. "ents" for measuring the performance of entity spans."pos" for measuring
             the performance of pos-tags. "token" for measuring the performance of tokenization. "nlp"
             for measuring the performance of all components in the specified nlp pipeline. Defaults to
             ["token", "pos", "ents"].
-        augmenter (Union[List[Callable], Callable], optional): A spaCy style augmenter which should be
+        augmenters (Union[List[Callable], Callable], optional): A spaCy style augmenter which should be
             applied to the corpus or a list thereof. defaults to [], indicating no augmenters.
         k (int, optional): Number of times it should run the augmentation and test the performance on
             the corpus. Defaults to 1.
@@ -96,6 +98,8 @@ def score(
         >>> scores.scores # extract dictionary of scores
         >>> scores.to_df() # creates a pandas dataframe of scores
     """
+
+
     if nlp is None:
         from spacy.lang.da import Danish
 
@@ -108,22 +112,39 @@ def score(
         "token": Scorer.score_tokenization,
         "nlp": scorer.score,
     }
-    corpus_ = copy(corpus)
-    if augmenter:
+    if callable(augmenters):
+        augmenters = [augmenters]
+    if len(augmenters) == 0:
+        augmenters = [dont_augment]
+
+    def __score(augmenter):
+        corpus_ = copy(corpus)
         corpus_.augmenter = augmenter
+        scores_ls = []
+        for i in range(k):
+            examples = [apply_fn(e) for e in corpus_(nlp)]
+            scores = {}
+            for fn in score_fn:
+                if isinstance(fn, str):
+                    fn = def_scorers[fn]
+                scores.update(fn(examples))
+            scores = flatten_dict(scores)
+            scores_ls.append(scores)
 
-    scores_ls = []
-    for i in range(k):
-        examples = [apply_fn(e) for e in corpus_(nlp)]
-        scores = {}
-        for fn in score_fn:
-            if isinstance(fn, str):
-                fn = def_scorers[fn]
-            scores.update(fn(examples))
-        scores = flatten_dict(scores)
-        scores_ls.append(scores)
+        # and collapse list to dict
+        for key in scores.keys():
+            scores[key] = [s[key] for s in scores_ls]
+        
+        scores["augmenter"] = [aug.__name__]*k
+        scores["k"] = list(range(k))
+        
+        return Scores(scores=scores)
+    
+    for i, aug in enumerate(augmenters):
+        score_ = __score(aug)
+        if i == 0:
+            score = score_
+        else:
+            score = score + score_
+    return score
 
-    # and collapse list to dict
-    for key in scores.keys():
-        scores[key] = [s[key] for s in scores_ls]
-    return Scores(scores=scores)
