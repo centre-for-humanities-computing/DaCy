9981067a44fbdf8036bf4d66f67996a77698307a
KennethEnevoldsen <kennethcenevolsen@gmail.com>
2021-07-10 15:24:51 +0200

    fixed tests

--- dacy/tests/test_score.py
+++ dacy/tests/test_score.py
@@ -1,5 +1,6 @@
 
 import spacy
+from spacy.lang.da import Danish
 import pandas as pd
 
 import dacy
@@ -10,11 +11,14 @@ from spacy.training.augment import create_lower_casing_augmenter
 
 
 def test_score():
-    nlp = dacy.load("da_dacy_medium_tft-0.0.0")
+    nlp = Danish()
 
-    def apply_model(example):
-        example.predicted = nlp(example.predicted.text)
-        return example
+    def apply_model(examples):
+        e = []
+        for example in examples:
+            example.predicted = nlp(example.predicted.text)
+            e.append(example)
+        return e
 
     test = dane(splits=["test"])
     test.limit = 1
@@ -40,11 +44,14 @@ def test_score():
         assert s in scores.columns
 
 def test_n_sents_score():
-    nlp = spacy.lang.da.Danish()
+    nlp = Danish()
 
-    def apply_model(example):
-        example.predicted = nlp(example.predicted.text)
-        return example
+    def apply_model(examples):
+        e = []
+        for example in examples:
+            example.predicted = nlp(example.predicted.text)
+            e.append(example)
+        return e
 
     scores = n_sents_score(
         n_sents=1,
