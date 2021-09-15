60d9caf080d80a85f0737b31200ed34144c53bc9
KennethEnevoldsen <kennethcenevolsen@gmail.com>
2021-09-15 16:04:26 +0200

    bumbed version number

--- dacy/about.py
+++ dacy/about.py
@@ -1,3 +1,3 @@
 __title__ = "dacy"
-__version__ = "1.1.3"  # the ONLY source of version ID
+__version__ = "1.1.4"  # the ONLY source of version ID
 __download_url__ = "https://github.com/centre-for-humanities-computing/DaCy"
