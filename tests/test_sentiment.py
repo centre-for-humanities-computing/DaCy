165e7678d594df6cead94ce3b68e05ec85f7ffb3
KennethEnevoldsen <kennethcenevolsen@gmail.com>
2022-06-27 13:21:32 +0200

    removed davader test

--- tests/test_sentiment.py
+++ tests/test_sentiment.py
@@ -1,11 +1,11 @@
 import spacy
 
-import dacy
+import dacy  # noqa
 
 
 def test_add_subjectivity():
     nlp = spacy.blank("da")
-    nlp.add_pipe("subjectivity")
+    nlp.add_pipe("dacy.subjectivity")
     texts = [
         "Analysen viser, at økonomien bliver forfærdelig dårlig",
         "Jeg tror alligevel, det bliver godt",
@@ -22,7 +22,7 @@ def test_add_subjectivity():
 
 def test_add_berttone_polarity():
     nlp = spacy.blank("da")
-    nlp.add_pipe("polarity")
+    nlp.add_pipe("dacy.polarity")
 
     texts = [
         "Analysen viser, at økonomien bliver forfærdelig dårlig",
@@ -36,44 +36,13 @@ def test_add_berttone_polarity():
 
 def test_add_bertemotion_laden():
     nlp = spacy.blank("da")
-    nlp.add_pipe("emotionally_laden")
+    nlp.add_pipe("dacy.emotionally_laden")
     doc = nlp("Der er et træ i haven")
     assert doc._.emotionally_laden == "no emotion"
 
 
 def test_add_bertemotion_emo():
     nlp = spacy.blank("da")
-    nlp.add_pipe("emotion")
+    nlp.add_pipe("dacy.emotion")
     doc = nlp("Har i set at Tesla har landet en raket på månen? Det er vildt!!")
     assert doc._.emotion == "overasket/målløs"
-
-
-def test_add_davader():
-    # using small danish spacy model as DaCy does not include a lemmatizer (yet)
-    try:
-        nlp = spacy.load("da_core_news_sm")
-    except OSError:
-        print(
-            "Downloading language model for the spaCy tokenization\n"
-            "(don't worry, this will only happen once)",
-        )
-        from spacy.cli import download
-
-        download("da_core_news_sm")
-    nlp = spacy.load("da_core_news_sm")
-
-    from functools import partial
-
-    from spacy.tokens import Doc
-
-    from dacy.sentiment import da_vader_getter
-
-    for func in [da_vader_getter, partial(da_vader_getter, lemmatization=False)]:
-        Doc.set_extension("vader_da", getter=func, force=True)
-
-        texts = ["Jeg er så glad", "jeg er rastløs"]
-
-        docs = list(nlp.pipe(texts))
-
-        assert docs[0]._.vader_da["compound"] > 0
-        assert docs[1]._.vader_da["neg"] > 0
