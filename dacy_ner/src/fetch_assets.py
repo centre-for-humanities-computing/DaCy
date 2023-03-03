b828f08e96f3ff40b821da458147de77858f4823
Emil Trenckner Jessen <emil.tj@hotmail.com>
2023-03-03 07:50:56 +0100

    dacy_ner

---
+++ dacy_ner/src/fetch_assets.py
@@ -0,0 +1,13 @@
+import sys
+
+
+def fetch_dansk():
+    # To be implemented once DANSK is publically released
+    print(
+        "DANSK has yet to be publically released before replication is possible - shutting down ..."
+    )
+    # sys.exit()
+
+
+if __name__ == "__main__":
+    fetch_dansk()
