b22e9d9f60c1e65f98f0b1dfb5318c033bd2eec8
Emil Trenckner Jessen <emil.tj@hotmail.com>
2023-03-09 11:08:23 +0100

    changed fetch_assets to download from hf hub, deleted split_dansk and updated requirements to include hf

--- ner_extended/fetch_assets.py
+++ ner_extended/fetch_assets.py
@@ -1,12 +1,30 @@
-import sys
+import spacy
+from spacy.tokens import DocBin, Doc
+from datasets import load_dataset
 
 
 def fetch_dansk():
-    # To be implemented once DANSK is publically released
-    print(
-        "DANSK has yet to be publically released before replication is possible - shutting down ...",
-    )
-    # sys.exit()
+    # DANSK has yet to publically released. Write the authors for a request of early read access.
+
+    # Download the datasetdict from the HuggingFace Hub
+    try:
+        datasets = load_dataset("chcaa/DANSK")
+    except FileNotFoundError:
+        print(
+            "\nERROR: DANSK has yet to publically released. Write the authors for a request of early read access. Shutting down.\n"
+        )
+        raise
+
+    nlp = spacy.blank("da")
+    partitions = ["train", "dev", "test"]
+    for p in partitions:
+        db = DocBin()
+        for doc in [
+            Doc(nlp.vocab).from_json(dataset_row) for dataset_row in datasets[f"{p}"]
+        ]:
+            db.add(doc)
+        db.to_disk(f"assets/{p}.spacy")
+        db.to_disk(f"corpus/{p}.spacy")
 
 
 if __name__ == "__main__":
