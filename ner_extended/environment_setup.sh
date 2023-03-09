048026d06f79f80bc1ab0aed6a52048da8986e9c
Emil Trenckner Jessen <emil.tj@hotmail.com>
2023-03-09 12:31:41 +0100

    added logins to the environment setup

--- ner_extended/environment_setup.sh
+++ ner_extended/environment_setup.sh
@@ -3,3 +3,5 @@ python3 -m venv environment/training_env
 source environment/training_env/bin/activate
 pip install -r "requirements.txt"
 pip install spacy[cuda101]
+huggingface-cli login
+wandb login
