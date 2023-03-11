cfce01645fa291023b4d96acb23f758f31bb2b03
Emil Trenckner Jessen <emil.tj@hotmail.com>
2023-03-11 08:29:50 +0100

    added cache dir for fetching dansk (too large file)

--- training/ner_fine_grained/fetch_assets.py
+++ training/ner_fine_grained/fetch_assets.py
@@ -6,7 +6,7 @@ from datasets import load_dataset
 def fetch_dansk():
     # Download the datasetdict from the HuggingFace Hub
     try:
-        datasets = load_dataset("chcaa/DANSK")
+        datasets = load_dataset("chcaa/DANSK", , cache_dir='assets')
     except FileNotFoundError:
         raise FileNotFoundError(
             "DANSK is not available. It might be due to either HuggingFace being down on the dataset not yet being publically released.",
