96f8741053ebe7dc6ca960b59b7df7b47b0540ca
KennethEnevoldsen <KennethEnevoldsen@users.noreply.github.com>
2026-08-27 11:56:17 +0000

    deploy: 68cdf1cc36d62d598ef166188acc23857779bc4d

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.8.1.dev1',
+    VERSION: '2.8.1.dev3',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
